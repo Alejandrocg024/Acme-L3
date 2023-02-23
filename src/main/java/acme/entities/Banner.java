54f482826cf2b3d178551a02b625a391b251f0fc
alegarsan11 <alegarsan11@alum.us.es>
2023-02-23 22:37:45 +0100

    Not null Constraint

--- src/main/java/acme/entities/Banner.java
+++ src/main/java/acme/entities/Banner.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 
 import org.hibernate.validator.constraints.Length;
@@ -23,13 +24,16 @@ public class Banner extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
+	@NotNull
 	@Temporal(value = TemporalType.TIMESTAMP)
 	@PastOrPresent
 	protected Date				instantiationMoment;
 
+	@NotNull
 	@Temporal(value = TemporalType.TIMESTAMP)
 	protected Date				displayPeriodBegin;
 
+	@NotNull
 	@Temporal(value = TemporalType.TIMESTAMP)
 	protected Date				displayPreriodFinish;
 
