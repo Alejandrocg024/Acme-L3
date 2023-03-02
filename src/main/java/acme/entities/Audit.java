a764d6259061d2cf0343b0ea101d2e2bd0eb8756
PabloMera6 <pabmergom@alum.us.es>
2023-03-02 11:15:49 +0100

    Student5-T5. Creación de la entidad Audit

--- src/main/java/acme/entities/Audit.java
+++ src/main/java/acme/entities/Audit.java
@@ -1,44 +1,51 @@
 
 package acme.entities;
 
-import java.util.List;
-
 import javax.persistence.Column;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
 import acme.datatypes.Mark;
 import acme.framework.data.AbstractEntity;
+import acme.roles.Auditor;
 
 public class Audit extends AbstractEntity {
 
-	protected static final long		serialVersionUID	= 1L;
+	protected static final long	serialVersionUID	= 1L;
 
 	@Column(unique = true)
 	@NotBlank
 	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
-	protected String				code;
+	protected String			code;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String				conclusion;
+	protected String			conclusion;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String				strongPoints;
+	protected String			strongPoints;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String				weakPoints;
+	protected String			weakPoints;
 
-	@OneToMany()
-	protected List<AuditingRecords>	auditingRecords;
+	@NotNull
+	protected Mark				mark;
 
+	@ManyToOne(optional = false)
+	@NotNull
+	@Valid
+	protected Course			course;
 
-	public Mark getMark() {
-		return null;
-	};
+	@ManyToOne(optional = false)
+	@NotNull
+	@Valid
+	protected Auditor			auditor;
+
 }
