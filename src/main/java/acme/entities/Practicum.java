47e6bd9c4bdaaea5198814ac24abdce215a586c5
davcorrom <davcorrom@alum.us.es>
2023-03-07 22:06:44 +0100

    Practicum entity created

---
+++ src/main/java/acme/entities/Practicum.java
@@ -0,0 +1,43 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Practicum extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			abstractInfo;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			goals;
+
+	@Digits(integer = 3, fraction = 2)
+	protected Double			estimatedTotalTime;
+
+}
