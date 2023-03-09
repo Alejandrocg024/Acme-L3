a999a2ef13be05992c2833288b10ba57da7f4fc6
PabloMera6 <pabmergom@alum.us.es>
2023-03-09 11:22:29 +0100

    fix Audit

--- src/main/java/acme/entities/Audit.java
+++ src/main/java/acme/entities/Audit.java
@@ -19,7 +19,7 @@ public class Audit extends AbstractEntity {
 
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}[0-9]{3}$")
 	protected String			code;
 
 	@NotBlank
@@ -34,8 +34,7 @@ public class Audit extends AbstractEntity {
 	@Length(max = 100)
 	protected String			weakPoints;
 
-	@NotBlank
-	protected String			mark;
+	protected boolean			draftMode;
 
 	@ManyToOne(optional = false)
 	@NotNull
