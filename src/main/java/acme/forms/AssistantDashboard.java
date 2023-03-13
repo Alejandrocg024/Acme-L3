fea1a6f2f473dd3648b483f2f119a84c143703c7
alegarsan11 <alegarsan11@alum.us.es>
2023-03-13 17:17:54 +0100

    Creation of AssistantDashboard

---
+++ src/main/java/acme/forms/AssistantDashboard.java
@@ -0,0 +1,21 @@
+
+package acme.forms;
+
+import java.util.Map;
+
+import acme.datatypes.Nature;
+import acme.datatypes.Statistic;
+import acme.framework.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AssistantDashboard extends AbstractForm {
+
+	private static final long	serialVersionUID	= 1L;
+
+	Map<Nature, Integer>		numOfTutorialsByType;
+	Statistic					metricsOfSessions;
+	Statistic					metricsOfTutorials;
+}
