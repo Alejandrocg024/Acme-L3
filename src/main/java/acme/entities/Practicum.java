197bae0b4e2f58fba445638607a19a3f08a6f0a0
davcorrom <davcorrom@alum.us.es>
2023-03-14 20:57:13 +0100

    Fixes in Practicum entity Fixes #237

--- src/main/java/acme/entities/Practicum.java
+++ src/main/java/acme/entities/Practicum.java
@@ -25,7 +25,7 @@ public class Practicum extends AbstractEntity {
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "^[A-Z]{1,3}[0-9]{3}$")
+	@Pattern(regexp = "^[A-Z]{1,3}\\d{3}$")
 	protected String			code;
 
 	@NotBlank
@@ -40,10 +40,7 @@ public class Practicum extends AbstractEntity {
 	@Length(max = 100)
 	protected String			goals;
 
-	/*
-	 * @Digits(integer = 3, fraction = 2)
-	 * protected Double estimatedTotalTime;
-	 */
+	protected boolean			draftMode;
 
 	@NotNull
 	@Valid
