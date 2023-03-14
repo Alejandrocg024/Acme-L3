d430c1b52ebf46e653f5a7f8eee042bf9d6614e0
alegarsan11 <alegarsan11@alum.us.es>
2023-03-14 17:18:27 +0100

    Fix: Pattern

--- src/main/java/acme/entities/Tutorial.java
+++ src/main/java/acme/entities/Tutorial.java
@@ -25,7 +25,7 @@ public class Tutorial extends AbstractEntity {
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "^[A-Z]{1,3}[0-9]{3}$")
+	@Pattern(regexp = "^[A-Z]{1,3}\\d{3}$")
 	protected String			code;
 
 	@NotBlank
