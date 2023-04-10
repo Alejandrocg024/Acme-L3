756002d7ad01db7348db67ee29291f64920f1c8e
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2023-04-10 13:31:01 +0200

    Group-T29 Comenzada

--- src/main/java/acme/components/AuxiliarService.java
+++ src/main/java/acme/components/AuxiliarService.java
@@ -1,7 +1,9 @@
 
 package acme.components;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +30,39 @@ public class AuxiliarService {
 		final SystemConfiguration sc = this.repository.findSystemConfiguration();
 		final SpamFilter spamFilter = new SpamFilter(sc.getSpamWords(), sc.getSpamThreshold());
 		return !spamFilter.isSpam(input);
+	}
+
+	public String translateBoolean(final boolean bool, final String lang) {
+		String res;
+		res = "";
+		if (lang.equals("en"))
+			res = bool ? "Yes" : "No";
+		else if (lang.equals("es"))
+			res = bool ? "Si" : "No";
+		return res;
+	}
+
+	public String translateDate(final Date date, final String lang) {
+		String res;
+		res = "";
+		final SimpleDateFormat spanishFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm");
+		res = spanishFormat.format(date);
+		if (lang.equals("en")) {
+			final SimpleDateFormat englishFormat = new SimpleDateFormat("yyyy/dd/MM hh:mm");
+			res = englishFormat.format(date);
+		}
+		return res;
+	}
+
+	public Money translateMoney(final Money money, final String lang) {
+		Money res;
+		res = new Money();
+		res.setAmount(money.getAmount());
+		if (lang.equals("en"))
+			res.setCurrency("USD");
+		else if (lang.equals("es"))
+			res.setCurrency("EUR");
+		return res;
 	}
 
 }
