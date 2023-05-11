d9fea8c7e574d97b23e57ea709c51137ed22f87b
PabloMera6 <pabmergom@alum.us.es>
2023-05-11 18:13:21 +0200

    fix Mejoras testing

--- src/test/java/acme/testing/auditor/auditingRecord/AuditorAuditingRecordCreateTest.java
+++ src/test/java/acme/testing/auditor/auditingRecord/AuditorAuditingRecordCreateTest.java
@@ -26,7 +26,7 @@ public class AuditorAuditingRecordCreateTest extends TestHarness {
 		super.signIn("auditor1", "auditor1");
 
 		super.clickOnMenu("Auditor", "My audits");
-		super.checkListingExists();
+		//super.checkListingExists();
 		super.sortListing(0, "asc");
 
 		super.clickOnListingRecord(auditRecordIndex);
@@ -41,27 +41,25 @@ public class AuditorAuditingRecordCreateTest extends TestHarness {
 		super.fillInputBoxIn("furtherInformationLink", furtherInformationLink);
 		super.clickOnSubmit("Create");
 
-		super.checkListingExists();
+		//super.checkListingExists();
 		super.sortListing(0, "asc");
 		super.checkColumnHasValue(auditingRecordRecordIndex, 0, subject);
 		super.checkColumnHasValue(auditingRecordRecordIndex, 1, assessment);
 
-		/*
-		 * super.clickOnListingRecord(auditingRecordRecordIndex);
-		 * super.checkInputBoxHasValue("subject", subject);
-		 * super.checkInputBoxHasValue("assessment", assessment);
-		 * super.checkInputBoxHasValue("startPeriod", startPeriod);
-		 * super.checkInputBoxHasValue("endPeriod", endPeriod);
-		 * super.checkInputBoxHasValue("mark", mark);
-		 * super.checkInputBoxHasValue("furtherInformationLink", furtherInformationLink);
-		 */
+		super.clickOnListingRecord(auditingRecordRecordIndex);
+		super.checkInputBoxHasValue("subject", subject);
+		super.checkInputBoxHasValue("assessment", assessment);
+		super.checkInputBoxHasValue("startPeriod", startPeriod);
+		super.checkInputBoxHasValue("endPeriod", endPeriod);
+		super.checkInputBoxHasValue("mark", mark);
+		super.checkInputBoxHasValue("furtherInformationLink", furtherInformationLink);
 
 		super.signOut();
 	}
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/auditor/auditing-record/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test200Negative(final int auditRecordIndex, final int auditingRecordRecordIndex, final String subject, final String assessment, final String startPeriod, final String endPeriod, final String mark, final String furtherInformationLink) {
+	public void test200Negative(final int auditRecordIndex, final String subject, final String assessment, final String startPeriod, final String endPeriod, final String mark, final String furtherInformationLink) {
 		// Este test intenta crear un registro de auditoría con datos incorrectos.
 
 		super.signIn("auditor1", "auditor1");
@@ -117,7 +115,7 @@ public class AuditorAuditingRecordCreateTest extends TestHarness {
 
 	public void test301Hacking() {
 		// Este test intenta crear un registro de auditoría
-		// para una auditoría que no fue creada por el principal
+		// para una auditoría que no fue creada por el auditor logeado
 
 		Collection<Audit> audits;
 		String param;
